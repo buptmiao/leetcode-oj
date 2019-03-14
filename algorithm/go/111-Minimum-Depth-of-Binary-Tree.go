32709084dabde9c44abf61e8dc6fd5b2aeb5d78f
miao <362622365@qq.com>
2019-03-14 16:05:58 +0800

    Add algorithm to 111

---
+++ algorithm/go/111-Minimum-Depth-of-Binary-Tree.go
@@ -0,0 +1,32 @@
+/**
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its minimum depth = 2.
+ */
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+
+	return int(math.Min(float64(minDepth(root.Left)), float64(minDepth(root.Right)))) + 1
+}
