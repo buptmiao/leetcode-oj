aa5e1695145f1424fe88929cacd600d3b259d841
miao <362622365@qq.com>
2019-04-10 14:07:08 +0800

    Add algorithm to 115

---
+++ algorithm/go/115-Distinct-Subsequences.go
@@ -0,0 +1,61 @@
+/**
+Given a string S and a string T, count the number of distinct subsequences of S which equals T.
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+Example 1:
+
+Input: S = "rabbbit", T = "rabbit"
+Output: 3
+Explanation:
+
+As shown below, there are 3 ways you can generate "rabbit" from S.
+(The caret symbol ^ means the chosen letters)
+
+rabbbit
+^^^^ ^^
+rabbbit
+^^ ^^^^
+rabbbit
+^^^ ^^^
+Example 2:
+
+Input: S = "babgbag", T = "bag"
+Output: 5
+Explanation:
+
+As shown below, there are 5 ways you can generate "bag" from S.
+(The caret symbol ^ means the chosen letters)
+
+babgbag
+^^ ^
+babgbag
+^^    ^
+babgbag
+^    ^^
+babgbag
+  ^  ^^
+babgbag
+    ^^^
+ */
+func numDistinct(s string, t string) int {
+	dp := make([][]int, len(t) + 1)
+	for i := range dp {
+		dp[i] = make([]int, len(s)+ 1)
+	}
+
+	for i := 0; i <= len(s); i++ {
+		dp[0][i] = 1
+	}
+
+	for i := 1; i <= len(t); i++ {
+		for j := i; j <= len(s); j++ {
+			if s[j-1] == t[i-1] {
+				dp[i][j] = dp[i][j-1] + dp[i-1][j-1]
+			} else {
+				dp[i][j] = dp[i][j-1]
+			}
+		}
+	}
+	return dp[len(t)][len(s)]
+}
